ee21ae80498b6b021db20adbee5f7e8c2f3c35ad
Huy <td.huycan@gmail.com>
2016-06-27 00:52:06 +0700

    refactor from controller to  url model

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,18 +1,15 @@
 class UrlsController < ApplicationController
   def show
-    full_url = Rails.cache.fetch("urls/#{ params[:code] }", expires_in: 60.days) do
-      (Url.find_by! code: params[:code]).full_url
-    end
+    url = Url.from params[:code]
 
     # enqueue for analytics
-    
 
-    redirect_to full_url
+
+    redirect_to url.full_url
   end
 
   def create
-    url = Url.find_or_create_by full_url: params[:url]
-    url.encode! and url.save if url.code.blank?
+    url = Url.find_or_new params[:url]
     render json: { url: url.shorten(base_url) }
   end
 
--- app/models/url.rb
+++ app/models/url.rb
@@ -7,6 +7,20 @@ class Url < ActiveRecord::Base
     self[:code] = encode self[:id]
   end
 
+  def self.from code
+    url = Rails.cache.fetch("#{ code }", expires_in: 60.days) do
+      Url.find_by! code: code
+    end
+
+    url
+  end
+
+  def self.find_or_new original_url
+    url = Url.find_or_create_by full_url: original_url
+    url.encode! and url.save if url.code.blank?
+    url
+  end
+
   private
   def code_map
     [*('a'..'z'), *('A'..'Z'), *('0'..'9')]
